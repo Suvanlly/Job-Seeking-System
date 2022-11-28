a69cb351ebe235fd0d2a2b527ff9cf863248c5da
Sophie Sun <swl381029812@gmail.com>
2022-11-28 10:44:00 +0800

    edit dashController

--- src/main/java/com/jssgui/gui/DashController.java
+++ src/main/java/com/jssgui/gui/DashController.java
@@ -1,18 +1,27 @@
 package com.jssgui.gui;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import org.w3c.dom.events.MouseEvent;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class DashController implements Initializable {
@@ -49,12 +58,48 @@ public class DashController implements Initializable {
     private ProfileController profileController;
 
 
+    @FXML
+    private ComboBox<String> comboCategory;
+
+    @FXML
+    private ComboBox<String> comboLocation;
+
+    @FXML
+    private ComboBox<String> comboSalary;
+
+    @FXML
+    private TextField searchBar;
+
+    @FXML
+    private String searchQuery;
+
+    @FXML
+    private String selectedCategory;
+
+    @FXML
+    private String selectedLocation;
+
+    @FXML
+    private String selectedSalary;
+
+
+
     UserInstance inst = UserInstance.getInstance();
 
-    public DashController(){}
+    public DashController(){
+
+    }
+
 
     public void initialize(URL location, ResourceBundle resources){
         labelTest.setText("Test - username: " + inst.getUser().getUsername() + " " + "typeID: " + inst.getUser().getUserTypeID()); //test
+        /*comboCategory.setItems(FXCollections.observableArrayList("Software Engineer","Business Analyst","Consultant", "Web Developer", "Network Engineer", "Product Manager", "IT Support","Test operator"));
+        comboLocation.setItems(FXCollections.observableArrayList("Melbourne","Sydney","Brisbane","Perth","Hobart","Remote"));
+        comboSalary.setItems(FXCollections.observableArrayList("Below $50000","$50000-$70000","$70000-$90000","$90000-$120000","$120000-$150000","Above $150000"));
+        */
+        populateCategory();
+        populateLocation();
+        populateSalary();
     }
 
     public void dashPress(ActionEvent event) {
@@ -109,6 +154,16 @@ public class DashController implements Initializable {
             }
         }
         else if (event.getSource() == buttonSearch) {
+
+            searchQuery = searchBar.getText();
+            handleComboSelections();
+
+            //testing value of combobox/search query
+            System.out.println(selectedCategory);
+            System.out.println(selectedLocation);
+            System.out.println(selectedSalary);
+            System.out.println(searchQuery);
+
             //labelHeader.setText(buttonSearch.getText());
             try {
                 Parent centerPane = FXMLLoader.load(Utility.class.getResource("Search.fxml"));
@@ -122,4 +177,60 @@ public class DashController implements Initializable {
             Utility.changeScene(event,"Login.fxml");
         }
     }
+
+    public void populateCategory() {
+        //change the list to retrieve from database
+        List<String> categories = Arrays.asList("category1", "category2", "category3");
+        for (String category : categories) {
+            comboCategory.getItems().add(category);
+        }
+        comboCategory.setValue("Select Category");
+    }
+
+    public void populateLocation() {
+        //change the list to retrieve from database
+        List<String> locations = Arrays.asList("location1", "location2", "location3");
+        for (String location : locations) {
+            comboLocation.getItems().add(location);
+        }
+        comboLocation.setValue("Select Location");
+    }
+
+    public void populateSalary() {
+        //change the list to retrieve from database
+        List<String> salaries = Arrays.asList("salary1", "salary2", "salary3");
+        for (String salary : salaries) {
+            comboSalary.getItems().add(salary);
+        }
+        comboSalary.setValue("Select Salary");
+    }
+
+    public void handleComboSelections() {
+        comboCategory.setOnAction(actionEvent -> {
+            selectedCategory = comboCategory.getSelectionModel().getSelectedItem();
+        });
+        comboLocation.setOnAction(actionEvent -> {
+            selectedLocation = comboLocation.getSelectionModel().getSelectedItem();
+        });
+        comboSalary.setOnAction(actionEvent-> {
+            selectedSalary = comboSalary.getSelectionModel().getSelectedItem();
+        });
+    }
+
+    public String getSearchQuery() {
+        return this.searchQuery;
+    }
+
+    public String getSelectedCategory() {
+        return this.selectedCategory;
+    }
+
+    public String getSelectedLocation() {
+        return this.selectedLocation;
+    }
+
+    public String getSelectedSalary() {
+        return this.selectedSalary;
+    }
+
 }
