8eb221c9fb631dc700f236959d01324c67579577
Sophie Sun <swl381029812@gmail.com>
2022-12-25 18:16:08 +1100

    jobsUtility

--- src/main/java/com/jssgui/gui/ProfileController.java
+++ src/main/java/com/jssgui/gui/ProfileController.java
@@ -1,15 +1,26 @@
 package com.jssgui.gui;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
 import java.net.URL;
-import java.sql.*;
-import java.text.DateFormat;
 import java.util.ResourceBundle;
 
 public class ProfileController implements Initializable{
+    @FXML
+    private Button buttonCompany;
+    @FXML
+    private Button buttonUpdate;
+
+    @FXML
+    private ScrollPane scrollProfile;
+
     @FXML
     private TextField tfFirst;
     @FXML
@@ -39,60 +50,344 @@ public class ProfileController implements Initializable{
     @FXML
     private TextField tfEdu;
     @FXML
-    private TextField tfCompletion;
+    private TextField tfGraduation;
     @FXML
     private TextField tfSkills;
 
     @FXML
-    private TextField tfCompany;
+    private Label labelPassMatch;
     @FXML
-    private TextField tfCompEmail;
-    @FXML
-    private TextField tfCompNo;
-    @FXML
-    private TextField tfCompAddress;
-    @FXML
-    private TextField tfCompCity;
-    @FXML
-    private TextField tfCompState;
-    @FXML
-    private TextField tfCompPost;
-    @FXML
-    private TextField tfCompCountry;
+    private Label labelUpdate;
 
     @FXML
     private DashController dashController;
 
+    @FXML
+    private ComboBox comboState;
+
     UserInstance inst = UserInstance.getInstance();
     public void initialize(URL location, ResourceBundle resources){
 
-        tfCompletion.disableProperty().bind(tfEdu.textProperty().isEmpty());
+        String[] states = {"Australian Capital Territory", "New South Wales", "Northern Territory", "Queensland", "South Australia", "Tasmania", "Victoria", "Western Australia"};
 
-        tfFirst.setText(inst.getUser().getFirstName());
-        tfLast.setText(inst.getUser().getLastName());
-        tfUser.setText(inst.getUser().getUsername());
-        tfPass.setText(inst.getUser().getPassword());
-        tfPassConfirm.setText(inst.getUser().getPassword());
-        tfEmail.setText(inst.getUser().getEmail());
-        tfDob.setText(String.format("%1$tY-%1$tm-%1$td", inst.getUser().getDateOfBirth()));
-        tfContact.setText(inst.getUser().getPhoneNo());
-        tfAddress.setText(inst.getUser().getAddress());
-        tfCity.setText(inst.getUser().getCity());
-        tfState.setText(inst.getUser().getState());
-        tfPost.setText(inst.getUser().getPost());
-        tfCountry.setText(inst.getUser().getCountry());
-        tfEdu.setText(inst.getUser().getEducation());
-        tfCompletion.setText(inst.getUser().getGraduation());
-        tfSkills.setText(inst.getUser().getSkills());
+        for (String inputStates : states) {
+            comboState.getItems().add(inputStates);
+        }
 
-        /*tfCompany.setText(inst.getUser().get);
-        tfCompEmail.setText(inst.getUser().get);
-        tfCompNo.setText(inst.getUser().get);
-        tfCompAddress.setText(inst.getUser().get);
-        tfCompCity.setText(inst.getUser().get);
-        tfCompState.setText(inst.getUser().get);
-        tfCompPost.setText(inst.getUser().get);
-        tfCompCountry.setText(inst.getUser().get);*/
+        tfGraduation.disableProperty().bind(tfEdu.textProperty().isEmpty());
+
+        if (inst.getType() == 1){
+            buttonCompany.setVisible(false);
+
+            tfFirst.setText(inst.getSeek().getFirstName());
+            tfLast.setText(inst.getSeek().getLastName());
+            tfUser.setText(inst.getSeek().getUsername());
+            tfPass.setText(inst.getSeek().getPassword());
+            tfPassConfirm.setText(inst.getSeek().getPassword());
+            tfEmail.setText(inst.getSeek().getEmail());
+            tfDob.setText(String.format("%1$td-%1$tm-%1$tY", inst.getSeek().getDateOfBirth()));
+            tfContact.setText(inst.getSeek().getPhoneNo());
+            tfAddress.setText(inst.getSeek().getAddress());
+            tfCity.setText(inst.getSeek().getCity());
+            comboState.getSelectionModel().select(inst.getSeek().getState());
+            tfPost.setText(inst.getSeek().getPost());
+            tfCountry.setText(inst.getSeek().getCountry());
+            tfEdu.setText(inst.getSeek().getEducation());
+            tfGraduation.setText(inst.getSeek().getGraduation());
+            tfSkills.setText(inst.getSeek().getSkills());
+        }
+        else{
+            buttonCompany.setVisible(true);
+
+            buttonCompany.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    try {
+                        BorderPane dash = (BorderPane) scrollProfile.getParent().getScene().getRoot();
+                        dash.setCenter(FXMLLoader.load(Utility.class.getResource("ProfileCompany.fxml")));
+                    }
+                    catch(IOException e){
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            tfFirst.setText(inst.getRec().getFirstName());
+            tfLast.setText(inst.getRec().getLastName());
+            tfUser.setText(inst.getRec().getUsername());
+            tfPass.setText(inst.getRec().getPassword());
+            tfPassConfirm.setText(inst.getRec().getPassword());
+            tfEmail.setText(inst.getRec().getEmail());
+            tfDob.setText(String.format("%1$td-%1$tm-%1$tY", inst.getRec().getDateOfBirth()));
+            tfContact.setText(inst.getRec().getPhoneNo());
+            tfAddress.setText(inst.getRec().getAddress());
+            tfCity.setText(inst.getRec().getCity());
+            comboState.getSelectionModel().select(inst.getRec().getState());
+//            tfState.setText(inst.getRec().getState());
+            tfPost.setText(inst.getRec().getPost());
+            tfCountry.setText(inst.getRec().getCountry());
+            tfEdu.setText(inst.getRec().getEducation());
+            tfGraduation.setText(inst.getRec().getGraduation());
+            tfSkills.setText(inst.getRec().getSkills());
+        }
+
+        buttonUpdate.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                String column;
+                String value;
+
+                if (inst.getType() == 1){
+                    if (!tfFirst.getText().equals(inst.getSeek().getFirstName())){
+                        column = "fname";
+                        value = tfFirst.getText();
+                        UpdateUtility.profileUp(column, value, tfUser.getText());
+
+                        inst.getSeek().setFirstName(value);
+                        scrollProfile.setVvalue(0);
+                        labelUpdate.setText("Updated Successfully.");
+                    }
+                    if (!tfLast.getText().equals(inst.getSeek().getLastName())){
+                        column = "lname";
+                        value = tfLast.getText();
+                        UpdateUtility.profileUp(column, value, tfUser.getText());
+
+                        inst.getSeek().setLastName(value);
+                        scrollProfile.setVvalue(0);
+                        labelUpdate.setText("Updated Successfully.");
+                    }
+                    if (!tfPass.getText().equals(inst.getSeek().getPassword())){
+                        column = "password";
+                        value = tfPass.getText();
+                        if (tfPassConfirm.getText().equals(tfPass.getText())){
+                            UpdateUtility.profileUp(column, value, tfUser.getText());
+
+                            inst.getSeek().setPassword(value);
+                            scrollProfile.setVvalue(0);
+                            labelPassMatch.setText("");
+                            labelUpdate.setText("Updated Successfully.");
+                        }
+                        else{
+                            scrollProfile.setVvalue(0);
+                            labelPassMatch.setText("Passwords Do Not Match. Please Try Again");
+                        }
+                    }
+                    if (!tfEmail.getText().equals(inst.getSeek().getEmail())){
+                        column = "email";
+                        value = tfEmail.getText();
+                        UpdateUtility.profileUp(column, value, tfUser.getText());
+
+                        inst.getSeek().setEmail(value);
+                        scrollProfile.setVvalue(0);
+                        labelUpdate.setText("Updated Successfully.");
+                    }
+                    if (!tfContact.getText().equals(inst.getSeek().getPhoneNo())){
+                        column = "contact";
+                        value = tfContact.getText();
+                        UpdateUtility.profileUp(column, value, tfUser.getText());
+
+                        inst.getSeek().setPhoneNo(value);
+                        scrollProfile.setVvalue(0);
+                        labelUpdate.setText("Updated Successfully.");
+                    }
+                    if (!tfAddress.getText().equals(inst.getSeek().getAddress())){
+                        column = "address";
+                        value = tfAddress.getText();
+                        UpdateUtility.profileUp(column, value, tfUser.getText());
+
+                        inst.getSeek().setAddress(value);
+                        scrollProfile.setVvalue(0);
+                        labelUpdate.setText("Updated Successfully.");
+                    }
+                    if (!tfCity.getText().equals(inst.getSeek().getCity())){
+                        column = "city";
+                        value = tfCity.getText();
+                        UpdateUtility.profileUp(column, value, tfUser.getText());
+
+                        inst.getSeek().setCity(value);
+                        scrollProfile.setVvalue(0);
+                        labelUpdate.setText("Updated Successfully.");
+                    }
+                    if (!comboState.getSelectionModel().getSelectedItem().equals(inst.getSeek().getState())){
+                        column = "state";
+                        value = comboState.getSelectionModel().getSelectedItem().toString();
+                        UpdateUtility.profileUp(column, value, tfUser.getText());
+
+                        inst.getSeek().setState(value);
+                        scrollProfile.setVvalue(0);
+                        labelUpdate.setText("Updated Successfully.");
+                    }
+                    if (!tfPost.getText().equals(inst.getSeek().getPost())){
+                        column = "post";
+                        value = tfPost.getText();
+                        UpdateUtility.profileUp(column, value, tfUser.getText());
+
+                        inst.getSeek().setPost(value);
+                        scrollProfile.setVvalue(0);
+                        labelUpdate.setText("Updated Successfully.");
+                    }
+                    if (!tfCountry.getText().equals(inst.getSeek().getCountry())){
+                        column = "country";
+                        value = tfCountry.getText();
+                        UpdateUtility.profileUp(column, value, tfUser.getText());
+
+                        inst.getSeek().setCountry(value);
+                        scrollProfile.setVvalue(0);
+                        labelUpdate.setText("Updated Successfully.");
+                    }
+                    if (!(tfEdu.getText() == null) && !tfEdu.getText().equals(inst.getSeek().getEducation())){
+                        column = "education";
+                        value = tfEdu.getText();
+                        UpdateUtility.profileUp(column, value, tfUser.getText());
+
+                        inst.getSeek().setEducation(value);
+                        scrollProfile.setVvalue(0);
+                        labelUpdate.setText("Updated Successfully.");
+                    }
+                    if (!(tfGraduation.getText() == null) && !tfGraduation.getText().equals(inst.getSeek().getGraduation())){
+                        column = "graduation";
+                        value = tfGraduation.getText();
+                        UpdateUtility.profileUp(column, value, tfUser.getText());
+
+                        inst.getSeek().setGraduation(value);
+                        scrollProfile.setVvalue(0);
+                        labelUpdate.setText("Updated Successfully.");
+                    }
+                    if (!(tfSkills.getText() == null) && !tfSkills.getText().equals(inst.getSeek().getSkills())){
+                        value = tfSkills.getText();
+                        UpdateUtility.skillUp(inst.getSeek().getUserID(), value);
+
+                        inst.getSeek().setSkills(value.toUpperCase());
+                        scrollProfile.setVvalue(0);
+                        labelUpdate.setText("Updated Successfully.");
+                    }
+                }
+                else{
+                    if (!tfFirst.getText().equals(inst.getRec().getFirstName())){
+                        column = "fname";
+                        value = tfFirst.getText();
+                        UpdateUtility.profileUp(column, value, tfUser.getText());
+
+                        inst.getRec().setFirstName(value);
+                        scrollProfile.setVvalue(0);
+                        labelUpdate.setText("Updated Successfully.");
+                    }
+                    if (!tfLast.getText().equals(inst.getRec().getLastName())){
+                        column = "lname";
+                        value = tfLast.getText();
+                        UpdateUtility.profileUp(column, value, tfUser.getText());
+
+                        inst.getRec().setLastName(value);
+                        scrollProfile.setVvalue(0);
+                        labelUpdate.setText("Updated Successfully.");
+                    }
+                    if (!tfPass.getText().equals(inst.getRec().getPassword())){
+                        column = "password";
+                        value = tfPass.getText();
+                        if (tfPassConfirm.getText().equals(tfPass.getText())){
+                            UpdateUtility.profileUp(column, value, tfUser.getText());
+
+                            inst.getRec().setPassword(value);
+                            scrollProfile.setVvalue(0);
+                            labelPassMatch.setText("");
+                            labelUpdate.setText("Updated Successfully.");
+                        }
+                        else{
+                            scrollProfile.setVvalue(0);
+                            labelPassMatch.setText("Passwords Do Not Match. Please Try Again");
+                        }
+                    }
+                    if (!tfEmail.getText().equals(inst.getRec().getEmail())){
+                        column = "email";
+                        value = tfEmail.getText();
+                        UpdateUtility.profileUp(column, value, tfUser.getText());
+
+                        inst.getRec().setEmail(value);
+                        scrollProfile.setVvalue(0);
+                        labelUpdate.setText("Updated Successfully.");
+                    }
+                    if (!tfContact.getText().equals(inst.getRec().getPhoneNo())){
+                        column = "contact";
+                        value = tfContact.getText();
+                        UpdateUtility.profileUp(column, value, tfUser.getText());
+
+                        inst.getRec().setPhoneNo(value);
+                        scrollProfile.setVvalue(0);
+                        labelUpdate.setText("Updated Successfully.");
+                    }
+                    if (!tfAddress.getText().equals(inst.getRec().getAddress())){
+                        column = "address";
+                        value = tfAddress.getText();
+                        UpdateUtility.profileUp(column, value, tfUser.getText());
+
+                        inst.getRec().setAddress(value);
+                        scrollProfile.setVvalue(0);
+                        labelUpdate.setText("Updated Successfully.");
+                    }
+                    if (!tfCity.getText().equals(inst.getRec().getCity())){
+                        column = "city";
+                        value = tfCity.getText();
+                        UpdateUtility.profileUp(column, value, tfUser.getText());
+
+                        inst.getRec().setCity(value);
+                        scrollProfile.setVvalue(0);
+                        labelUpdate.setText("Updated Successfully.");
+                    }
+                    if (!comboState.getSelectionModel().getSelectedItem().equals(inst.getRec().getState())){
+                        column = "state";
+                        value = comboState.getSelectionModel().getSelectedItem().toString();
+                        UpdateUtility.profileUp(column, value, tfUser.getText());
+
+                        inst.getRec().setState(value);
+                        scrollProfile.setVvalue(0);
+                        labelUpdate.setText("Updated Successfully.");
+                    }
+                    if (!tfPost.getText().equals(inst.getRec().getPost())){
+                        column = "post";
+                        value = tfPost.getText();
+                        UpdateUtility.profileUp(column, value, tfUser.getText());
+
+                        inst.getRec().setPost(value);
+                        scrollProfile.setVvalue(0);
+                        labelUpdate.setText("Updated Successfully.");
+                    }
+                    if (!tfCountry.getText().equals(inst.getRec().getCountry())){
+                        column = "country";
+                        value = tfCountry.getText();
+                        UpdateUtility.profileUp(column, value, tfUser.getText());
+
+                        inst.getRec().setCountry(value);
+                        scrollProfile.setVvalue(0);
+                        labelUpdate.setText("Updated Successfully.");
+                    }
+                    if (!(tfEdu.getText() == null) && !tfEdu.getText().equals(inst.getRec().getEducation())){
+                        column = "education";
+                        value = tfEdu.getText();
+                        UpdateUtility.profileUp(column, value, tfUser.getText());
+
+                        inst.getRec().setEducation(value);
+                        scrollProfile.setVvalue(0);
+                        labelUpdate.setText("Updated Successfully.");
+                    }
+                    if (!(tfGraduation.getText() == null) && !tfGraduation.getText().equals(inst.getRec().getGraduation())){
+                        column = "graduation";
+                        value = tfGraduation.getText();
+                        UpdateUtility.profileUp(column, value, tfUser.getText());
+
+                        inst.getRec().setGraduation(value);
+                        scrollProfile.setVvalue(0);
+                        labelUpdate.setText("Updated Successfully.");
+                    }
+                    if (!(tfSkills.getText() == null) && !tfSkills.getText().equals(inst.getRec().getSkills())){
+                        value = tfSkills.getText();
+                        UpdateUtility.skillUp(inst.getRec().getUserID(), value);
+
+                        inst.getRec().setSkills(value.toUpperCase());
+                        scrollProfile.setVvalue(0);
+                        labelUpdate.setText("Updated Successfully.");
+                    }
+                }
+            }
+        });
     }
-
 }
